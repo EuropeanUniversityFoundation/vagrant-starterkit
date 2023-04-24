8e6b740ec8b90d11ad3b09348776e2553dcaaded
Tiago Simões <tiago.simoes@uni-foundation.eu>
2023-04-24 16:50:10 +0200

    Makes git setup completely dependent on ENV vars

--- .provision.d/10_git_setup.sh
+++ .provision.d/10_git_setup.sh
@@ -6,25 +6,29 @@ apt-get install git -y
 # Global configuration for the system user, including GitHub access.
 GITCONFIG_FILE=/home/vagrant/.gitconfig
 
-cp -p ${VAGRANT_ROOT}/dotfiles/example.gitconfig ${GITCONFIG_FILE}
-
 if [[ ! -z ${GIT_NAME} ]]; then
-  sed -i 's/"John Doe"/'"${GIT_NAME}"'/' ${GITCONFIG_FILE}
+  sudo -u vagrant git config --file ${GITCONFIG_FILE} \
+  --add user.name "${GIT_NAME}"
 fi
 
 if [[ ! -z ${GIT_EMAIL} ]]; then
-  sed -i 's/john.doe@example.com/'"${GIT_EMAIL}"'/' ${GITCONFIG_FILE}
+  sudo -u vagrant git config --file ${GITCONFIG_FILE} \
+  --add user.email "${GIT_EMAIL}"
 fi
 
 if [[ ! -z ${GITHUB_USERNAME} ]]; then
-  sed -i 's/jdoe/'"${GITHUB_USERNAME}"'/' ${GITCONFIG_FILE}
+  sudo -u vagrant git config --file ${GITCONFIG_FILE} \
+  --add github.username "${GITHUB_USERNAME}"
 fi
 
 if [[ ! -z ${GITHUB_TOKEN} ]]; then
-  sed -i 's/PAT/'"${GITHUB_TOKEN}"'/' ${GITCONFIG_FILE}
+  sudo -u vagrant git config --file ${GITCONFIG_FILE} \
+  --add github.token "${GITHUB_TOKEN}"
 fi
 
-# Establish SSH access to GitHub.
-cp -p /home/vagrant/github-ssh/* /home/vagrant/.ssh/
-# Connect to GitHub; prevent script from exiting.
-sudo -u vagrant ssh -T -o "StrictHostKeyChecking no" git@github.com || true
+if [[ ! -z ${GITHUB_SSH_DIR} ]]; then
+  # Establish SSH access to GitHub.
+  cp -p /home/vagrant/github-ssh/* /home/vagrant/.ssh/
+  # Connect to GitHub; prevent script from exiting.
+  sudo -u vagrant ssh -T -o "StrictHostKeyChecking no" git@github.com || true
+fi
