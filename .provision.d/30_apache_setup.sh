b0f0f3fb44622955bc4ccdc5f2fbf50cb425ab27
Tiago Simões <tiago.simoes@uni-foundation.eu>
2023-04-25 18:57:03 +0200

    Switches Apache to alternative ports before Nginx is installed

--- .provision.d/30_apache_setup.sh
+++ .provision.d/30_apache_setup.sh
@@ -9,8 +9,12 @@ mkdir /var/www/html/apache2
 mv /var/www/html/index.html /var/www/html/apache2/
 
 # Update default VirtualHosts and reload.
-find /etc/apache2/sites-available -name *.conf -exec \
-sed -i 's,/var/www/html,/var/www/html/apache2,g' {} \;
+APACHE_DEFAULT=/etc/apache2/sites-available/000-default.conf
+APACHE_DEFAULT_SSL=/etc/apache2/sites-available/default-ssl.conf
+
+sed -i 's,/var/www/html,/var/www/html/apache2,g' ${APACHE_DEFAULT}
+sed -i 's,/var/www/html,/var/www/html/apache2,g' ${APACHE_DEFAULT_SSL}
+
 service apache2 reload
 
 # Check for existing SSL certificates for current FQDN.
@@ -25,13 +29,24 @@ if [[ -d ${SSL_CERTS}/${HOSTNAME_F} ]]; then
     # Replace the snakeoil certificate.
     SNAKEOIL_PEM=/etc/ssl/certs/ssl-cert-snakeoil.pem
     SNAKEOIL_KEY=/etc/ssl/private/ssl-cert-snakeoil.key
-    DEFAULT_SSL=/etc/apache2/sites-available/default-ssl.conf
 
-    sed -i 's,'"${SNAKEOIL_PEM}"','"${SSL_PEM}"',g' ${DEFAULT_SSL}
-    sed -i 's,'"${SNAKEOIL_KEY}"','"${SSL_KEY}"',g' ${DEFAULT_SSL}
+    sed -i 's,'"${SNAKEOIL_PEM}"','"${SSL_PEM}"',g' ${APACHE_DEFAULT_SSL}
+    sed -i 's,'"${SNAKEOIL_KEY}"','"${SSL_KEY}"',g' ${APACHE_DEFAULT_SSL}
 
     a2enmod ssl
     a2ensite default-ssl.conf
     service apache2 restart
   fi
+fi
+
+# Switch to alternative ports to prevent conflicts with Nginx.
+if [[ ! -z ${NGINX_SETUP} ]]; then
+  APACHE_PORTS=/etc/apache2/ports.conf
+
+  sed -i 's,80,'"${APACHE2_ALT_HTTP:-8080}"',g' ${APACHE_PORTS}
+  sed -i 's,80,'"${APACHE2_ALT_HTTP:-8080}"',g' ${APACHE_DEFAULT}
+  sed -i 's,443,'"${APACHE2_ALT_HTTPS:-8443}"',g' ${APACHE_PORTS}
+  sed -i 's,443,'"${APACHE2_ALT_HTTPS:-8443}"',g' ${APACHE_DEFAULT_SSL}
+
+  service apache2 restart
 fi
